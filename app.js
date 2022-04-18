72890e6f7648292b394aaa2b720b07c011640919
haymanmk <haymanmk@gmail.com>
2022-04-18 18:59:54 +0800

    upload manuals

--- app.js
+++ app.js
@@ -1,25 +1,25 @@
 //Connection Manager automagically reconnects to controller that have lost connection. Scanner auto initiated.
 
-const {ControllerManager} = require('st-ethernet-ip')
+// const {ControllerManager} = require('st-ethernet-ip')
 
-const cm = new ControllerManager();
+// const cm = new ControllerManager();
 
-//addController(ipAddress, slot = 0, rpi = 100, connected = true, retrySP = 3000, opts = {})
-const cont = cm.addController('172.16.0.10');
+// //addController(ipAddress, slot = 0, rpi = 100, connected = true, retrySP = 3000, opts = {})
+// const cont = cm.addController('172.16.0.10');
 
-cont.connect();
+// cont.connect();
 
 
-//addTag(tagname, program = null, arrayDims = 0, arraySize = 0x01)
-cont.addTag('Output')
+// //addTag(tagname, program = null, arrayDims = 0, arraySize = 0x01)
+// cont.addTag('Output')
 
-cont.on('TagChanged', (tag, prevValue) => {
-  console.log(tag.name, ' changed from ', prevValue, ' => ', tag.value)
-})
+// cont.on('TagChanged', (tag, prevValue) => {
+//   console.log(tag.name, ' changed from ', prevValue, ' => ', tag.value)
+// })
 
-cont.on('error', (e) => {
-  console.log(e)
-})
+// cont.on('error', (e) => {
+//   console.log(e)
+// })
 
 
 
@@ -72,3 +72,53 @@ cont.on('error', (e) => {
 //     // 'device' is the disconnected device
 //     console.log('Device Disconnected');
 // });
+
+
+const { IO } = require("st-ethernet-ip")
+
+const scanner = new IO.Scanner(); // Iinitalize new scanner on default port 2222
+
+// device configuration from manufacturer.
+const config = {
+  configInstance: {
+    assembly: 10,
+    size: 0
+  },
+  outputInstance: {
+    assembly: 101,
+    size: 2
+  },
+  inputInstance: {
+    assembly: 10,
+    size: 2
+  }
+}
+
+// Add a connection with (device config, rpi, ip_address)
+const conn = scanner.addConnection(config, 100, '172.16.0.10')
+
+// After first UDP packet is received
+conn.on('connected', () => {
+  console.log('Connected')
+  console.log('input data => ', conn.inputData) // Display Input Data Buffer.
+  
+  console.log('output data => ', conn.outputData) // Display Output Data Buffer.
+  // Create alias for bits and integers (can be named what ever you want)
+//   conn.addOutputBit(4, 0, 'out0') // Using outputData. Skip 4 bytes, use bit 0 and call it 'out0'
+//   conn.addOutputBit(4, 1, 'out1') // Skip 4 bytes, use bit 1 and call it 'out1'
+//   conn.addOutputInt(2, 'outputValue0') // Skip 2 bytes then use 16bit integer and call it 'value0'
+//   conn.addInputBit(4, 7, 'in7') // Skip 4 bytes then use bit 7 and call it 'in7'
+//   conn.addInputInt(2, 'inputValue0')
+//   // Set values to be written to devices
+//   conn.setValue('out0', true) 
+//   conn.setValue('out1', false)
+//   conn.setValue('value0', 1234)
+//   // Read values from device connection
+//   console.log(conn.getValue('in7'))
+//   console.log(conn.getValue('inputValue0'))
+// })
+
+// // Called when UDP packets are not receiving. (Timeout is based on rpi setting)
+// conn.on('disconnected', () => {
+//   console.log('Disconnected')
+})
