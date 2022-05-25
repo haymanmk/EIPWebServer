792aaeadac3075cd0f894ffda2f1b332c1187539
haymanmk <haymanmk@gmail.com>
2022-05-25 11:52:10 +0800

    add server listener

--- app.js
+++ app.js
@@ -5,13 +5,13 @@
 // const cm = new ControllerManager();
 
 // //addController(ipAddress, slot = 0, rpi = 100, connected = true, retrySP = 3000, opts = {})
-// const cont = cm.addController('172.16.0.10');
+// const cont = cm.addController('192.168.0.10');
 
 // cont.connect();
 
 
 // //addTag(tagname, program = null, arrayDims = 0, arraySize = 0x01)
-// cont.addTag('Output')
+// cont.addTag('Tag_1')
 
 // cont.on('TagChanged', (tag, prevValue) => {
 //   console.log(tag.name, ' changed from ', prevValue, ' => ', tag.value)
@@ -74,51 +74,86 @@
 // });
 
 
-const { IO } = require("st-ethernet-ip")
+// const { IO } = require("st-ethernet-ip")
 
-const scanner = new IO.Scanner(); // Iinitalize new scanner on default port 2222
+// const scanner = new IO.Scanner(); // Iinitalize new scanner on default port 2222
 
-// device configuration from manufacturer.
-const config = {
-  configInstance: {
-    assembly: 10,
-    size: 0
-  },
-  outputInstance: {
-    assembly: 101,
-    size: 2
-  },
-  inputInstance: {
-    assembly: 10,
-    size: 2
-  }
-}
+// // device configuration from manufacturer.
+// const config = {
+//   // configInstance: {
+//   //   assembly: 10,
+//   //   size: 0
+//   // },
+//   outputInstance: {
+//     assembly: 100,
+//     size: 400
+//   },
+//   inputInstance: {
+//     assembly: 101,
+//     size: 240
+//   }
+// }
 
-// Add a connection with (device config, rpi, ip_address)
-const conn = scanner.addConnection(config, 100, '172.16.0.10')
+// // Add a connection with (device config, rpi, ip_address)
+// const conn = scanner.addConnection(config, 100, '192.168.0.10')
 
-// After first UDP packet is received
-conn.on('connected', () => {
-  console.log('Connected')
-  console.log('input data => ', conn.inputData) // Display Input Data Buffer.
+// // After first UDP packet is received
+// conn.on('connected', () => {
+//   console.log('Connected')
+//   console.log('input data => ', conn.inputData) // Display Input Data Buffer.
   
-  console.log('output data => ', conn.outputData) // Display Output Data Buffer.
-  // Create alias for bits and integers (can be named what ever you want)
-//   conn.addOutputBit(4, 0, 'out0') // Using outputData. Skip 4 bytes, use bit 0 and call it 'out0'
-//   conn.addOutputBit(4, 1, 'out1') // Skip 4 bytes, use bit 1 and call it 'out1'
-//   conn.addOutputInt(2, 'outputValue0') // Skip 2 bytes then use 16bit integer and call it 'value0'
-//   conn.addInputBit(4, 7, 'in7') // Skip 4 bytes then use bit 7 and call it 'in7'
-//   conn.addInputInt(2, 'inputValue0')
-//   // Set values to be written to devices
-//   conn.setValue('out0', true) 
-//   conn.setValue('out1', false)
-//   conn.setValue('value0', 1234)
-//   // Read values from device connection
-//   console.log(conn.getValue('in7'))
-//   console.log(conn.getValue('inputValue0'))
+//   console.log('output data => ', conn.outputData) // Display Output Data Buffer.
+//   // Create alias for bits and integers (can be named what ever you want)
+//   // conn.addOutputBit(4, 0, 'out0') // Using outputData. Skip 4 bytes, use bit 0 and call it 'out0'
+//   // conn.addOutputBit(4, 1, 'out1') // Skip 4 bytes, use bit 1 and call it 'out1'
+//   // conn.addOutputInt(2, 'outputValue0') // Skip 2 bytes then use 16bit integer and call it 'value0'
+//   // conn.addInputBit(4, 7, 'in7') // Skip 4 bytes then use bit 7 and call it 'in7'
+//   // conn.addInputInt(2, 'inputValue0')
+//   // // Set values to be written to devices
+//   // conn.setValue('out0', true) 
+//   // conn.setValue('out1', false)
+//   // conn.setValue('value0', 1234)
+//   // // Read values from device connection
+//   // console.log(conn.getValue('in7'))
+//   // console.log(conn.getValue('inputValue0'))
 // })
 
 // // Called when UDP packets are not receiving. (Timeout is based on rpi setting)
 // conn.on('disconnected', () => {
 //   console.log('Disconnected')
-})
+// })
+const net = require("net");
+const {header} = require("st-ethernet-ip/src/enip/encapsulation");
+
+const server = net.createServer((socket)=>{
+  console.log("connected");
+  socket.on("data", (data)=>{
+    console.log("data received: "+Buffer.isBuffer(data));
+    if(Buffer.isBuffer(data)){
+      const encapsulatedData = header.parse(data);
+      const {commandCode, statusCode} = encapsulatedData;
+
+      console.log(commandCode, statusCode);
+    }
+  })
+});
+
+server.listen(44818, ()=>{
+  console.log('server bound');
+});
+
+server.on("connection", ()=>{
+  console.log("client requests connetion");
+});
+
+
+
+
+
+// const {EthernetIP} = require("st-ethernet-ip");
+
+// console.log(EthernetIP);
+
+// EthernetIP.on("Session Registered", (id)=>{
+//   console.log(id);
+// });
